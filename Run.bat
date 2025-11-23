e71881329d89848c92ec6090d86be266d9a6a47f
Sumit1694 <Sumitrane198@gmail.com>
2025-11-23 23:20:18 +0530

    Run configuration changes

---
+++ Run.bat
@@ -0,0 +1,2 @@
+C:\Users\Sumit\eclipse-workspace\NewAccount02092025\cucumber-selenium-tutorialsninja
+mvn test
--- src/test/java/runners/TestNGRTestRunner.java
+++ src/test/java/runners/TestNGRTestRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/features",glue="stepDefinitions",monochrome=true,dryRun=false,tags="@Regression",
+@CucumberOptions(features="src/test/java/features",glue="stepDefinitions",monochrome=true,dryRun=false,tags="@TC_ATC_001",
 plugin={"pretty","html:target/cucumber-report.html","json:target/cucumber-jsonreport.json","rerun:target/failed_scenarios.txt","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 public class TestNGRTestRunner extends AbstractTestNGCucumberTests {
